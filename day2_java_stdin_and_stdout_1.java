d095c6cbc3ad9cc83b3edaee451b610a56902dc2
DarmiDahlan <99661645+DarmiDahlan@users.noreply.github.com>
2022-02-20 21:46:03 +0800

    day2_java_stdin_and_stdout_1.java

---
+++ day2_java_stdin_and_stdout_1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+    public static void main(String[] args) {
+        
+        Scanner scanf = new Scanner(System.in);
+        int a = scanf.nextInt();
+        int b = scanf.nextInt();
+        int c = scanf.nextInt();
+        
+        System.out.println(a); 
+        System.out.println(b); 
+        System.out.println(c); 
+    }
+}
